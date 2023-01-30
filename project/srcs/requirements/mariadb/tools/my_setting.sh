bb22745a439d2f0de19a91d1d0be6e76fc72363f
Your Name <you@example.com>
2023-01-30 20:53:17 +0300

    not_work

--- project/srcs/requirements/mariadb/tools/my_setting.sh
+++ project/srcs/requirements/mariadb/tools/my_setting.sh
@@ -18,12 +18,11 @@ if [ ! -d "/var/lib/mysql/wordpress" ]; then
     sleep 1
 
         mysql -e "CREATE DATABASE $DB_NAME;"
-        mysql -e "CREATE USER 'cpollito'@'%' IDENTIFIED BY '123';"
-        mysql -e "GRANT ALL ON wordpress.* TO cpollito@'%';"
+        mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+        mysql -e "GRANT ALL ON $DB_NAME.* TO $DB_USER@'%';"
         # mysql -e "GRANT ALL ON `wordpress`.* TO 'cpollito'@'%';"
         # echo GRANT ALL ON `wordpress`.* TO 'cpollito'@%; | mysql
         mysql -e "FLUSH PRIVILEGES;"
-        mysqladmin --socket=/tmp/mysqld.sock -u root password 123
+        mysqladmin --socket=/tmp/mysqld.sock -u root password $MYSQL_ROOT_PASSWORD
     
 fi
-
