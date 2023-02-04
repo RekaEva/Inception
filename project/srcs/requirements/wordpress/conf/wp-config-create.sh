63380b81f0905b8f2b7168bdaeef5d9558efe49c
Your Name <you@example.com>
2023-02-04 13:23:01 +0300

    website

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -14,4 +14,23 @@ wp core install --allow-root --path=/var/www/wordpress --url=$DOMAIN_NAME \
 wp user create --allow-root --path=/var/www/wordpress/ $WP_USER wp@m.ru \
                 --role=author --user_pass=$WP_USER_PASS
 
+if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
+wp theme install inspiro --activate --allow-root
+
+# enable redis cache
+    sed -i "40i define( 'WP_REDIS_HOST', '$REDIS_HOST' );"      wp-config.php
+    sed -i "41i define( 'WP_REDIS_PORT', 6379 );"               wp-config.php
+    #sed -i "42i define( 'WP_REDIS_PASSWORD', '$REDIS_PWD' );"   wp-config.php
+    sed -i "42i define( 'WP_REDIS_TIMEOUT', 1 );"               wp-config.php
+    sed -i "43i define( 'WP_REDIS_READ_TIMEOUT', 1 );"          wp-config.php
+    sed -i "44i define( 'WP_REDIS_DATABASE', 0 );\n"            wp-config.php
+
+    wp plugin install redis-cache --activate --allow-root
+    wp plugin update --all --allow-root
+
+echo "Wordpress: set up!"
+fi
+
+wp redis enable --allow-root
+
 exec /usr/sbin/php-fpm81 -F
